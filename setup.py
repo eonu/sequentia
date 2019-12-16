31799d81950760a909e6b13ad06090f0bd4981e6
Edwin Onuonga <edwinonuonga@gmail.com>
2019-12-16 23:12:01 +0000

    [patch:pkg] Standardize single quotes (#9)
    * Standardize single quotes

--- setup.py
+++ setup.py
@@ -5,41 +5,41 @@ from setuptools import setup, find_packages
 
 VERSION = '0.1.0.alpha'
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name = "sequentia",
+    name = 'sequentia',
     version = VERSION,
-    author = "Edwin Onuonga",
-    author_email = "ed@eonu.net",
-    description = "A machine learning interface for isolated temporal sequence classification algorithms in Python.",
+    author = 'Edwin Onuonga',
+    author_email = 'ed@eonu.net',
+    description = 'A generalized interface for isolated temporal sequence classification using Hidden Markov Models and Dynamic Time Warping kNN.',
     long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/eonu/sequentia",
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/eonu/sequentia',
     project_urls = {
-        "Bug Tracker": "https://github.com/eonu/sequentia/issues",
-        "Source Code": "https://github.com/eonu/sequentia"
+        'Bug Tracker': 'https://github.com/eonu/sequentia/issues',
+        'Source Code': 'https://github.com/eonu/sequentia'
     },
     license = 'MIT',
     package_dir = {'': 'lib'},
     packages = find_packages(where='lib'),
     classifiers = [
-        "Development Status :: 1 - Planning",
-        "Programming Language :: Python :: 3.5",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Science/Research",
-        "Topic :: Software Development",
-        "Topic :: Scientific/Engineering",
-        "Natural Language :: English"
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Natural Language :: English'
     ],
+    python_requires='>=3.5',
     install_requires = [
         'numpy>=1.17,<2',
         'pomegranate>=0.11,<1',
         'fastdtw>=0.3,<0.4',
         'scipy>=1.3,<2',
         'scikit-learn>=0.22,<1'
-    ],
-    python_requires='>=3.5'
+    ]
 )
