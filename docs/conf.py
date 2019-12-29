2bacd1d31cd9ec7d48f9dd1367c32bef1bd780f8
Edwin Onuonga <edwinonuonga@gmail.com>
2019-12-29 20:34:59 +0400

    Release v0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019-2020, Edwin Onuonga'
 author = 'Edwin Onuonga'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.0'
 
 # -- General configuration ---------------------------------------------------
 
