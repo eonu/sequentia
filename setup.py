a1a3f892d7b79acff3832e1f24ea83e6f00983b3
Edwin Onuonga <edwinonuonga@gmail.com>
2020-01-05 23:45:04 +0400

    Release v0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 from setuptools import setup, find_packages
 
-VERSION = '0.5.0'
+VERSION = '0.6.0'
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
