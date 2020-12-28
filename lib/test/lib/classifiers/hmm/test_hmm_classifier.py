2b1d30126a4b9fe7240f6dc0098cf6e6ed4a4828
Edwin Onuonga <edwinonuonga@gmail.com>
2020-12-29 03:09:13 +0400

    Add docstrings to test cases

--- lib/test/lib/classifiers/hmm/test_hmm_classifier.py
+++ lib/test/lib/classifiers/hmm/test_hmm_classifier.py
@@ -63,14 +63,17 @@ def test_predict_single_frequency_prior():
     assert prediction == 4
 
 def test_predict_single_uniform_prior():
+    """Predict a single observation sequence with a uniform prior"""
     prediction = hmm_clf.predict(x, prior='uniform', return_scores=False, original_labels=False)
     assert prediction == 4
 
 def test_predict_single_custom_prior():
+    """Predict a single observation sequence with a custom prior"""
     prediction = hmm_clf.predict(x, prior=([1-4e-10]+[1e-10]*4), return_scores=False, original_labels=False)
     assert prediction == 0
 
 def test_predict_single_return_scores():
+    """Predict a single observation sequence and return the transformed label, with the un-normalized posterior scores"""
     prediction = hmm_clf.predict(x, prior='frequency', return_scores=True, original_labels=False)
     assert isinstance(prediction, tuple)
     assert prediction[0] == 4
@@ -79,10 +82,12 @@ def test_predict_single_return_scores():
     ]))
 
 def test_predict_single_original_labels():
+    """Predict a single observation sequence and return the original label, without the un-normalized posterior scores"""
     prediction = hmm_clf.predict(x, prior='uniform', return_scores=False, original_labels=True)
     assert prediction == 'c4'
 
 def test_predict_single_return_scores_original_labels():
+    """Predict a single observation sequence and return the original label, with the un-normalized posterior scores"""
     prediction = hmm_clf.predict(x, prior='frequency', return_scores=True, original_labels=True)
     assert isinstance(prediction, tuple)
     assert prediction[0] == 'c4'
@@ -96,14 +101,17 @@ def test_predict_multiple_frequency_prior():
     assert_equal(predictions, np.array([4, 4, 0]))
 
 def test_predict_multiple_uniform_prior():
+    """Predict multiple observation sequences with a uniform prior"""
     predictions = hmm_clf.predict(X, prior='uniform', return_scores=False, original_labels=False)
     assert_equal(predictions, np.array([4, 4, 0]))
 
 def test_predict_multiple_custom_prior():
+    """Predict multiple observation sequences with a custom prior"""
     predictions = hmm_clf.predict(X, prior=([1-4e-10]+[1e-10]*4), return_scores=False, original_labels=False)
     assert_equal(predictions, np.array([0, 0, 0]))
 
 def test_predict_multiple_return_scores():
+    """Predict multiple observation sequences and return the transformed labels, with the un-normalized posterior scores"""
     predictions = hmm_clf.predict(X, prior='frequency', return_scores=True, original_labels=False)
     assert isinstance(predictions, tuple)
     assert_equal(predictions[0], np.array([4, 4, 0]))
@@ -114,10 +122,12 @@ def test_predict_multiple_return_scores():
     ]))
 
 def test_predict_multiple_original_labels():
+    """Predict multiple observation sequences and return the original labels, without the un-normalized posterior scores"""
     predictions = hmm_clf.predict(X, prior='frequency', return_scores=False, original_labels=True)
     assert all(np.equal(predictions.astype(object), np.array(['c4', 'c4', 'c0'], dtype=object)))
 
 def test_predict_multiple_return_scores_original_labels():
+    """Predict multiple observation sequences and return the original labels, with the un-normalized posterior scores"""
     predictions = hmm_clf.predict(X, prior='frequency', return_scores=True, original_labels=True)
     assert isinstance(predictions, tuple)
     assert all(np.equal(predictions[0].astype(object), np.array(['c4', 'c4', 'c0'], dtype=object)))
@@ -132,6 +142,7 @@ def test_predict_multiple_return_scores_original_labels():
 # ======================== #
 
 def test_evaluate():
+    """Evaluate performance on some observation sequences and labels"""
     acc, cm = hmm_clf.evaluate(X, Y, prior='frequency')
     assert acc == 0.0
     assert_equal(cm, np.array([
@@ -147,6 +158,7 @@ def test_evaluate():
 # ===================================== #
 
 def test_serialization():
+    """Serialize and deserialize the classifier"""
     model_file = 'test.pkl'
     try:
         with open(model_file, 'wb') as file:
