32629327e140004e05fff8222c4458875d9f9e78
Edwin Onuonga <edwinonuonga@gmail.com>
2020-12-30 13:59:20 +0400

    SandboxViolation fix

--- setup.py
+++ setup.py
@@ -3,6 +3,24 @@
 from __future__ import print_function
 from setuptools import setup, find_packages
 
+def fix_setuptools():
+    """Work around bugs in setuptools.
+
+    Some versions of setuptools are broken and raise SandboxViolation for normal
+    operations in a virtualenv. We therefore disable the sandbox to avoid these
+    issues.
+    """
+    try:
+        from setuptools.sandbox import DirectorySandbox
+           def violation(operation, *args, **_):
+            print "SandboxViolation: %s" % (args,)
+
+        DirectorySandbox._violation = violation
+    except ImportError:
+        pass
+# Fix bugs in setuptools.
+fix_setuptools()
+
 VERSION = '0.10.0'
 
 with open('README.md', 'r') as fh:
