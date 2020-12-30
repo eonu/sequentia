b16dc2244711ae96c07095e86cd6703da9204284
Edwin Onuonga <edwinonuonga@gmail.com>
2020-12-30 13:41:09 +0400

    Add scipy to setup_requires

--- setup.py
+++ setup.py
@@ -42,7 +42,8 @@ setup(
     python_requires='>=3.6,<3.10',
     setup_requires = [
         'Cython',
-        'numpy>=1.17,<2'
+        'numpy>=1.17,<2',
+        'scipy>=1.3,<2'
     ],
     install_requires = [
         'numpy>=1.17,<2',
