d81e51ef2247df5f4b7ca93c1826324946f1af8e
Edwin Onuonga <edwinonuonga@gmail.com>
2022-06-26 21:17:51 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 import platform
 from pkg_resources import packaging
 
-VERSION = '0.13.0'
+VERSION = '0.13.1'
 
 with open('README.md', 'r', encoding='utf8') as fh:
     long_description = fh.read()
