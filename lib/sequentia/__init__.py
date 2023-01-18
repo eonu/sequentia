7135ef046542a87a0ca8f6b8d0aafb35600f54a9
Edwin Onuonga <edwinonuonga@gmail.com>
2023-01-18 01:13:07 +0000

    bump version and changelog

--- lib/sequentia/__init__.py
+++ lib/sequentia/__init__.py
@@ -5,7 +5,7 @@ from .pipeline import *
 from .utils import *
 
 __name__ = "sequentia"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Edwin Onuonga"
 __email__ = "ed@eonu.net"
 __copyright__ = f"2019-2023, {__author__}"
