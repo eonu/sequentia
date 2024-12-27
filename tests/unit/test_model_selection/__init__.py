6661ff94d517c21be978db302a17362f153a62e6
Edwin Onuonga <edwinonuonga@gmail.com>
2024-12-27 15:59:48 +0000

    add unit tests

--- tests/unit/test_model_selection/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2019 Sequentia Developers.
-# Distributed under the terms of the MIT License (see the LICENSE file).
-# SPDX-License-Identifier: MIT
-# This source code is part of the Sequentia project (https://github.com/eonu/sequentia).
-
