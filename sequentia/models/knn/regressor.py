5a8802632fd30fe518acdac3657a679a79ebb5c1
Edwin Onuonga <edwinonuonga@gmail.com>
2024-12-30 12:35:54 +0000

    set `use_c=True` by default for `KNNClassifier`/`KNNRegressor`

--- sequentia/models/knn/regressor.py
+++ sequentia/models/knn/regressor.py
@@ -39,7 +39,7 @@ class KNNRegressor(KNNMixin, RegressorMixin):
         weighting: t.Callable[[FloatArray], FloatArray] | None = None,
         window: pyd.confloat(ge=0.0, le=1.0) = 1.0,
         independent: bool = False,
-        use_c: bool = False,
+        use_c: bool = True,
         n_jobs: pyd.PositiveInt | pyd.NegativeInt = 1,
         random_state: pyd.NonNegativeInt | np.random.RandomState | None = None,
     ) -> pyd.SkipValidation:
